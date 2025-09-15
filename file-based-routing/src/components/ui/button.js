5533aa0a8adfb41d93071d40a73d6789f29d21b3
Advant ayush <ayush.kava@advant.xyz>
2025-09-15 19:05:35 +0530

    Learn pre page rendering and catch dynamic route

--- file-based-routing/src/components/ui/button.js
+++ file-based-routing/src/components/ui/button.js
@@ -2,9 +2,15 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 export default function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <button className={classes.btn}>{props.children}</button>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      <button className={classes.btn}>{props.children}</button>
-    </Link>
-  );
+    <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
+  )
 }
--- file-based-routing/src/pages/events/[...slug].js
+++ file-based-routing/src/pages/events/[...slug].js
@@ -1,11 +1,53 @@
-import React from 'react'
+import {useRouter} from 'next/router'
+import { getFilteredEvents } from '../../../dummy-data';
+import EventList from '../../components/events/event-list';
+import { Fragment } from 'react';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
 
 export default function EventFilter() {
+  const router = useRouter();
+  const FilteredData = router.query.slug;
+
+  if(!FilteredData){
+    return <p className='center'>Loading...</p>
+  }
+
+  const year = FilteredData[0];
+  const month = FilteredData[1];
+
+  const numYear = +year;
+  const numMonth = +month;
+
+  if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12){
+    return <Fragment>
+      <div className='center'> 
+      <p>Invalid Filter. Please adjust your values!</p>
+        <Button link='/events'>
+          Show All Events
+        </Button>
+      </div>
+    </Fragment>
+  }
+
+  const filteredEvents = getFilteredEvents({year: numYear, month: numMonth});
+  if(!filteredEvents || filteredEvents.length === 0){
+    return <Fragment>
+      <div className='center'> 
+      <p>No events found for the chosen filter!</p>
+        <Button link='/events'>
+          Show All Events
+        </Button>
+      </div>
+    </Fragment>
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
-        <h1>
-            Event Filter Page
-        </h1>
-    </div>
+    <Fragment>
+        <ResultsTitle date={date} />
+        <EventList items={filteredEvents}/> 
+    </Fragment>
   )
 }
